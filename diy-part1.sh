e46224c510971c175f9a783a5492e84ee9d55bf1
sukareki <sukareki@hotmail.com>
2021-02-15 00:52:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/master
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
